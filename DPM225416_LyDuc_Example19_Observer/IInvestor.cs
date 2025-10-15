be2aa4e700b86a8882cb144f74dd4700677fa6e6
Enishi-G <duc_dpm225416@student.agu.edu.vn>
2025-10-15 12:52:29 +0700

    Add files via upload

---
+++ DPM225416_LyDuc_Example19_Observer/IInvestor.cs
@@ -0,0 +1,12 @@
+﻿namespace Observer.RealWorld
+{
+    /// <summary>
+    /// The 'Observer' interface
+    /// </summary>
+
+    public interface IInvestor
+    {
+        void Update(Stock stock);
+    }
+}
+
---
+++ DPM225416_LyDuc_Patern19_Observer/Observer.cs
@@ -0,0 +1,12 @@
+﻿namespace Observer.Structural
+{
+    /// <summary>
+    /// The 'Observer' abstract class
+    /// </summary>
+
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+}
+
